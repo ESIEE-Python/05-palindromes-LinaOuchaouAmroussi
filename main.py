e85ce0ddba7728653506209f3df82635c20308ed
LinaOuchaouAmroussi <lina.ouchaou@edu.esiee.fr>
2024-09-21 10:54:13 +0000

    Code de l'exercice sur les palidromes

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-#### Fonction secondaire
-
-
-def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
-
-#### Fonction principale
-
-
+"""
+Cette fonction teste si il s'agit d'un palindrome ou pas"""
+def ispalindrome(s):
+    """Cette fonction este si s est un palindrome
+    :param s: la chaine de caractères
+    :return "s est un palindrome et false sinon
+    """
+    accents = "àéèùîïçêâôöëûüÂÊÎÔÛÄËÏÖÜÀÇÉÈÙ"
+    sans_accents = "aeeuiiceaooeuuAEIOUAEIOUACEEU"
+    trantab = str.maketrans(accents, sans_accents)
+    s = s.translate(trantab)
+    ponctuation = ".,!?;:«»'\"-()[]{}"
+    trantab_punct = str.maketrans('', '', ponctuation)
+    s = s.translate(trantab_punct)
+    s = s.replace(" ", "")
+    s = s.lower()
+    return s  == s[::-1]
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """ 
+    Fonction principale pour tester si ce sont des palindromes ou pas"""
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
 
